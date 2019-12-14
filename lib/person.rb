e219e3fdf8141027c2825a8aeee3f4c9c729257c
JoelBly <jtb6179@g.rit.edu>
2019-12-13 20:14:14 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+class Person
+    #attr_accessor :breed
+
+    def initialize
+        @name = name 
+        @job = job
+    end 
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+       @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+       @job
+    end
+end
